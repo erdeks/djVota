9bc8887af9206fc5b621a7ee7ad52046f9b1093b
erdeks <aponcelasgarcia@iesesteveterradas.cat>
2018-01-25 17:10:04 +0100

    usuarios creados

--- djangoVota/polls/admin.py
+++ djangoVota/polls/admin.py
@@ -13,6 +13,11 @@ class PreguntaAdmin(admin.ModelAdmin):
     list_display = ('texto', 'fecha_publicacion')
     list_filter = ['fecha_publicacion']
     search_fields = ['texto']
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(autor=request.user)
 admin.site.register(Pregunta, PreguntaAdmin)
 admin.site.register(Respuesta, RespuestaAdmin)
 admin.site.register(Voto)
